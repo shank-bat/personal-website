0c80fcb98249396581accdc22e6d6aba1e6ea69e
Shashank <shashank.bhat2k05@gmail.com>
2025-10-15 22:45:08 +0530

    Switch to root domain config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/personal-website',
-  assetPrefix: '/personal-website/',
   eslint: {
     ignoreDuringBuilds: true,
   },
